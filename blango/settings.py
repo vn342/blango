0fbc33621e949a42855d2ff634cfb4afecc8ff91
Valentina Nardo <valentina.nardo@capstaninc.us>
2023-02-18 03:01:30 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -86,6 +86,30 @@ class Dev(Configuration):
       },
   ]
 
+
+  LOGGING = {
+      "version": 1,
+      "disable_existing_loggers": False,
+      "formatters": {
+          "verbose": {
+              "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+              "style": "{",
+        },
+      },
+      "handlers": {
+          "console": {
+              "class": "logging.StreamHandler",
+              "stream": "ext://sys.stdout",
+              "formatter": "verbose",
+          },
+      },
+      "root": {
+          "handlers": ["console"],
+          "level": "DEBUG",
+      },
+  }
+
+
   WSGI_APPLICATION = 'blango.wsgi.application'
 
 
--- blog/views.py
+++ blog/views.py
@@ -5,11 +5,16 @@ from blog.models import Post
 
 from django.shortcuts import redirect
 from blog.forms import CommentForm
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 
 # Create your views here.
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
@@ -23,6 +28,7 @@ def post_detail(request, slug):
                 comment.content_object = post
                 comment.creator = request.user
                 comment.save()
+                logger.info("Created comment on Post %d for user %s", post.pk, request.user)
                 return redirect(request.path_info)
         else:
             comment_form = CommentForm()
