37247b79106f42a95149c034ff1cb881fbc56c53
Valentina Nardo <valentina.nardo@capstaninc.us>
2023-02-27 03:22:25 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -73,6 +73,8 @@ class Dev(Configuration):
 
   INTERNAL_IPS = ["192.168.11.179"]
   AUTH_USER_MODEL = "blango_auth.User"
+  EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+  ACCOUNT_ACTIVATION_DAYS = 7
 
   ROOT_URLCONF = 'blango.urls'
 
