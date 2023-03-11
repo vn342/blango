7f78231cd43982b25e2983c74172e385b365dea2
Valentina Nardo <valentina.nardo@capstaninc.us>
2023-03-11 20:30:56 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -52,13 +52,26 @@ class Dev(Configuration):
       'django.contrib.contenttypes',
       'django.contrib.sessions',
       'django.contrib.messages',
+      'django.contrib.sites',
       'django.contrib.staticfiles',
       'blango_auth',
       'blog',
       'crispy_forms',
       'crispy_bootstrap5',
       'debug_toolbar',
+      'allauth',
+      'allauth.account',
+      'allauth.socialaccount',
+      'allauth.socialaccount.providers.google',
   ]
+
+  SITE_ID = 1
+
+  ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+  ACCOUNT_EMAIL_REQUIRED = True
+  ACCOUNT_USERNAME_REQUIRED = False
+  ACCOUNT_AUTHENTICATION_METHOD = "email"
+
 
   MIDDLEWARE = [
       'django.middleware.security.SecurityMiddleware',
